34ac7b1476902ef46f956c57b44a293adc8ff706
hurricanenara <naralee0124@gmail.com>
2023-07-10 02:17:54 -0400

    specify node verison

--- app/controllers/api/portfo_data_controller.rb
+++ app/controllers/api/portfo_data_controller.rb
@@ -6,12 +6,15 @@ class Api::PortfoDataController < ApplicationController
         @current_time = Time.current
         @data
         if range == "1D"
+            @data = PortfoDatum.where(created_at: @current_time.yesterday.all_day).where(user_id: @current_user.id).order('id ASC')
             if @current_time.saturday?
                 @data = PortfoDatum.where(created_at: @current_time.yesterday.all_day).where(user_id: @current_user.id).order('id ASC')
                 # @data = PortfoDatum.where('created_at >= ?', 1.days.ago).where(user_id: @current_user.id).order('id ASC')
             elsif @current_time.sunday?
-            #    @data = PortfoDatum.where('created_at >= ?', 2.days.ago).where(user_id: @current_user.id).order('id ASC')
-            @data = PortfoDatum.where(created_at: @current_time.yesterday.yesterday.all_day).where(user_id: @current_user.id).order('id ASC')
+                #    @data = PortfoDatum.where('created_at >= ?', 2.days.ago).where(user_id: @current_user.id).order('id ASC')
+                @data = PortfoDatum.where(created_at: @current_time.yesterday.yesterday.all_day).where(user_id: @current_user.id).order('id ASC')
+            elsif @current_time.monday && @current_time.hour < 9
+                @data = PortfoDatum.where(created_at: @current_time.yesterday.yesterday.yesterday.all_day).where(user_id: @current_user.id).order('id ASC')
             else
                 @data = PortfoDatum.where(created_at: Time.current.all_day, user_id: @current_user.id).order('created_at ASC')
             end
